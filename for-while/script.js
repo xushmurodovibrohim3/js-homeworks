aec32f31d91fb0576b385d787a8ee59555b84a13
Xushmuratov Ibrohim <xushmurodovibrohim3@gmail.com>
2023-05-27 12:06:10 +0500

    feat: add function findMinNumber

--- for-while/script.js
+++ for-while/script.js
@@ -1,8 +1,23 @@
+// let a = +prompt("Enter your age: ");
+// let condition = (a >= 1 && a <= 127) ? false : true;
+// alert(condition)
+// while (condition) {
+//     a = +prompt("Please, try again: ")
+//     condition = (a >= 1 && a >= 127) ? false : true;
+// }
 let arr = [-10 , -4 , -4 , 3 , 2 , 4 , -10];
 let counter = 0;
 let resault1 = 0;
 let resault2 = 0;
 let resault3;
+let resault4;
+function findMinNumber(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        resault4 = arr[0];
+        resault4 = (resault4 < arr[i]) ? arr[i] : resault4;
+    }
+    return resault4;
+}
 for(let i = 0; i < arr.length; i++){
     let a = i;
     resault1 += arr[i];
@@ -18,3 +33,4 @@ for(let i = 0; i < arr.length; i++){
 }
 console.log("sum of numbers: " + resault1)
 console.log("sum of negative numbers: " + -resault2)
+console.log("Smallest number: " + findMinNumber(arr))
