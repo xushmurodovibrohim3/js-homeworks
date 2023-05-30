1a4d7b23feb7973937edf9df2bee0ab405d603d0
Xushmuratov Ibrohim <xushmurodovibrohim3@gmail.com>
2023-05-30 22:01:28 +0500

    feat: create math methods

---
+++ number-methods/script.js
@@ -0,0 +1,51 @@
+function getIntegerNumber(n) {
+   let a = n + "";
+   let resault = "";
+   for (let i = 0; i < a.length; i++) {
+      if (a[i] !== ".") {
+         resault += a[i]
+      } else {
+         i = a.length
+      }
+   }
+   return +resault
+}
+
+function numFloor(n) {
+   return (n > 0) ? getIntegerNumber(n) : getIntegerNumber(n) - 1
+}
+
+function numRound(n) {
+   if (n > 0) {
+      return (n - getIntegerNumber(n) > 0.5) ? getIntegerNumber(n) + 1 : getIntegerNumber(n)
+   } else {
+      return (n + getIntegerNumber(n) > -0.5) ? getIntegerNumber(n) : getIntegerNumber(n) - 1
+   }
+}
+
+function numCeil(n) {
+   return (n > 0) ? getIntegerNumber(n) + 1 : getIntegerNumber(n);
+}
+
+function sqrt(n) {
+   return n ** (1 / 2)
+}
+
+function pow(n = 1, d = 0) {
+   return n ** d
+}
+
+let Math = {
+   floor: numFloor,
+   round: numRound,
+   ceil: numCeil,
+   trunc: getIntegerNumber,
+   pow: pow,
+   sqrt: sqrt
+}
+
+let num = {
+
+}
+
+console.log(Math.round(7.4));
